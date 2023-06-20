a06422fd04c2f2d87d238d4067b3db989038a3f5
Garrett Becker <garrettdbecker@gmail.com>
2023-06-20 11:19:04 -0400

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # gen10Dev10PreTrainingProject
-Review project before fall 2019 training: ToDo List Assignment
+
+Before starting Genesis10's Dev10 Java bootcamp, everyone in my cohort and I worked on a review project from the quick course we went through during the interview process: a small To-Do list app. This app takes in the task you want to accomplish and either the number of days it will take to finish (that's V1) or the due date you want to have it done by (V2). The list of your tasks appear below in colors indicating priority level. I made both versions to test myself with different kinds of form inputs and use JavaScript date functions.
+
+### Demo Views
+
+V1 - Input the number of days
+
+![](./todo-list-v1.jpg)
+
+V2 - Input the due date
+
+![](./todo-list-v2.jpg)
+
+### Links
+
+- [Live Demo V1](https://replit.com/@gdbecker/Pre-Training-Project-V1)
+- [Live Demo V2](https://replit.com/@gdbecker/Pre-Training-Project-V2)
+
+## Author
+
+- Website - [Garrett Becker]()
+- Replit - [@gdbecker](https://replit.com/@gdbecker)
+- LinkedIn - [Garrett Becker](https://www.linkedin.com/in/garrett-becker-923b4a106/)
--- todolist2.js
+++ todolist2.js
@@ -1,74 +1,75 @@
 //Function for clearing errors of main form
-function clearErrors() {    
+function clearErrors() {
     for (var loopCounter = 0; loopCounter < document.forms["todolist"].elements.length; loopCounter++) {
-        if (document.forms["todolist"].elements[loopCounter].parentElement.className.indexOf("has-") != -1) {
-            document.forms["todolist"].elements[loopCounter].parentElement.className = "form-group";
-        }
-    }    
-} 
-
-//Primary function for ToDo List assignment
-//Attached to "Submit" button
-function submitTask() {
+      if (document.forms["todolist"].elements[loopCounter].parentElement.className.indexOf("has-") != -1) {
+        document.forms["todolist"].elements[loopCounter].parentElement.className = "form-group";
+      }
+    }
+  }
+  
+  //Primary function for ToDo List assignment
+  //Attached to "Submit" button
+  function submitTask() {
     clearErrors();
-    
+  
     //Collect Task description and number of days to complete
     //Check to make sure that these are valid values for each field
     var task = document.forms["todolist"]["task"].value;
-    if (task.value == "" || task.length == 0 || task == null) 
-    {
-        alert("Please fill out the 'Task' field.");
-        document.forms["todolist"]["task"]
-           .parentElement.className = "form-group has-error";
-        document.forms["todolist"]["task"].focus();
-        return false;
+    if (task.value == "" || task.length == 0 || task == null) {
+      alert("Please fill out the 'Task' field.");
+      document.forms["todolist"]["task"]
+        .parentElement.className = "form-group has-error";
+      document.forms["todolist"]["task"].focus();
+      return false;
     }
-    
+  
     //Get tomorrow's date
     var today = new Date()
     var tomorrow = new Date(today)
     tomorrow.setDate(tomorrow.getDate() + 1)
-    
+  
     //Format tomorrow's 
-    
+  
     var date = new Date();
-    date = document.getElementById("date").valueAsDate;    
+    date = document.getElementById("date").valueAsDate;
+  
+    //Calculate days to complete the task
+    var daysToComplete = Number(Math.round((date - today) / (1000 * 60 * 60 * 24))) + 1
+  
     if (date == "") {
-        alert("Please fill out this field");
-        document.forms["todolist"]["date"]
-           .parentElement.className = "form-group has-error";
-        document.forms["todolist"]["date"].focus();
-        return false;
-    } else if (dates.compare(date,tomorrow)==-1) {
-        alert("Value must be " + tomorrow + " or later.");
-        document.forms["todolist"]["date"]
-           .parentElement.className = "form-group has-error";
-        document.forms["todolist"]["date"].focus();
-        return false;        
-    } 
-    
+      alert("Please fill out this field");
+      document.forms["todolist"]["date"]
+        .parentElement.className = "form-group has-error";
+      document.forms["todolist"]["date"].focus();
+      return false;
+    } else if (daysToComplete < 0) {
+      alert("Value must be " + tomorrow.toLocaleDateString("en-US") + " or later.");
+      document.forms["todolist"]["date"]
+        .parentElement.className = "form-group has-error";
+      document.forms["todolist"]["date"].focus();
+      return false;
+    }
+  
     //Define key variables and initialize them
     var taskDescription = task; //String variable for taskDescription
-    var daysToComplete = 0; //String variable for daysToComplete
-    daysToComplete = Number(days); //Number variable for daysToComplete
-    
+  
     //Define the type of alert to show to screen based on number of days to complete task
     var alertType = null;
-    
-    if ((daysToComplete < 7) && (daysToComplete < 3)) {
-        alertType = "alert alert-danger alert-dismissible"; //red
+  
+    if (daysToComplete < 3) {
+      alertType = "alert alert-danger alert-dismissible"; //red
     } else if ((daysToComplete < 7) && (daysToComplete >= 3)) {
-        alertType = "alert alert-warning alert-dismissible"; //yellow
+      alertType = "alert alert-warning alert-dismissible"; //yellow
     } else {
-        alertType = "alert alert-secondary alert-dismissible"; //gray
+      alertType = "alert alert-secondary alert-dismissible"; //gray
     }
-    
+  
     //Append new alert code to the existing div
     var div = document.getElementById("alerts");
-    div.innerHTML += '<div class="'+alertType+'" role="alert" style="margin-left: 7.5px; margin-right: 7.5px; margin-bottom: 15px">'+taskDescription+'<br/>'+daysToComplete+' day(s)<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-    
+    div.innerHTML += '<div class="' + alertType + '" role="alert" style="margin-left: 7.5px; margin-right: 7.5px; margin-bottom: 15px">' + taskDescription + '<br/>Due: ' + date.toLocaleDateString("en-US") + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+  
     document.forms["todolist"]["task"].value = "";
-    document.forms["todolist"]["days"].value = "";
-    
+    document.forms["todolist"]["date"].value = "";
+  
     return false;
-}
+  }
